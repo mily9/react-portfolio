6a909298999809e7615c33d7fb7ac0c1fa653947
Emilia Ashraghi <emiliaashraghi@gmail.com>
2022-04-16 13:10:24 -0400

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import "./portfolio.css"
 
 const portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My recent work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/mily9" className='btn'>Github</a>
+          <a href="https://github.com/mily9" className='btn btn-primary'>Github</a>
+
+        </article>
+      </div>
+    </section>
   )
 }
 
